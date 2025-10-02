dbdb6ad95081f6ca302faf317f49006a407936be
jackdaniel-jd <vigopattaraphong@gmail.com>
2025-10-02 22:47:06 +0700

    haha

--- javascripts/testjs.js
+++ javascripts/testjs.js
@@ -1,1 +1,14 @@
-alert('HAHAHAHAHA')
+let input = prompt("Input integer number");
+let number = parseInt(input);
+
+if (isNaN(number)) {
+    alert("Invalid input! Please enter an integer number.");
+} else {
+    if (number === 0) {
+        alert("zero integer number");
+    } else if (number % 2 === 0) {
+        alert("even integer number");
+    } else {
+        alert("odd integer number");
+    }
+}
